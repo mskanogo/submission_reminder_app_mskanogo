0847cd88756f4c47d9661eed1f859f08ae2d5f00
mskanogo <m.kanogo@alustudent.com>
2025-10-16 16:31:42 +0200

    Remove environment folders from main branch

--- submission_reminder_stacey/app/reminder.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-source ./config/config.env
-source ./modules/functions.sh
-
-echo "Assignment: $ASSIGNMENT"
-echo "Days remaining to submit: $DAYS_REMAINING days"
-echo "--------------------------------------------"
-
-check_submissions ./assets/submissions.txt
